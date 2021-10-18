acd5f0ff32b76ae86148223d91f3a84cd29aa2e5
Brian Lowther <merchanthagen@gmail.com>
2021-10-18 12:03:58 -0700

    Not pretty, but functional single play game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,19 @@
 /* the first move always starts with player X
 the app detects a win or tie and displays an appropriate message
 a button resets the game for a new round of gameplay */
-
+let clickCounter = 0;
+const squares = document.getElementsByTagName('td');
+for(let square of squares) {
+  square.addEventListener('click', ()=>{
+    if(square.innerHTML.length === 0) {
+      if(clickCounter === 0 || clickCounter % 2 === 0) {
+        square.innerHTML = 'X';
+        clickCounter++;
+      } else {
+          square.innerHTML = 'O';
+          clickCounter++;
+      }
+    }
+  });
+}
 
